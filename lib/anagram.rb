03e4ca60d7b980acac52dd72959f45f9488cdbd2
Luke Pierotti <lpierotti2@gmail.com>
2017-07-02 08:34:21 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
+require "pry"
 # Your code goes here!
+class Anagram 
+
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def to_param(word)
+		word.split("").sort
+		#binding.pry
+	end
+
+
+	def match(anagrams)
+		search_word = to_param(self.word)
+		anagrams.select do |word|
+			possible_match = to_param(word)
+			search_word == possible_match
+		end
+	end
+end
